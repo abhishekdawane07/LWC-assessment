e75b2a21023bdfb7b8a37492c6a0484a33081e42
abhishekdawane07 <85093278+abhishekdawane07@users.noreply.github.com>
2024-07-02 21:10:06 +0530

    Delete lwc directory

--- lwc/appoinmentform/appoinmentform.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement } from 'lwc';
-export default class AppointmentForm extends LightningElement {
-handleSubmit(event) {
-    event.preventDefault(); // Prevent default form submission
-    const fields = event.detail.fields; // Get the fields values
-    this.template.querySelector('lightning-record-edit-form').submit(fields); // Submit the form with the field values
-}
-}
